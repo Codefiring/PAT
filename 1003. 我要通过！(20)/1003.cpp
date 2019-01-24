fd37db775ff4230e979e3ef3ff205734e3ab2589
codefiring <hhx970127@163.com>
2019-01-24 20:53:51 +0800

    change

---
+++ 1003. 我要通过！(20)/1003.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int str_num;
+    cin >> str_num;
+    string str;
+    for(int i = 0;i < str_num;i++)
+        cin>>str;
+        for(int j = 0;j < str.size();j++)
+         
+
+
+    
+    return 0;
+}
