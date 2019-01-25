62a83f4241d30a0597fd2a369a8e11d06f2f88e3
codefiring <hhx970127@163.com>
2019-01-25 20:26:53 +0800

    1005

---
+++ 1005 继续(3n+1)猜想（25）/1005.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	vector<int> numbers;
+	int size,num;
+	cin >> size;
+	while (cin >> num)
+		numbers.push_back(num);
+
+}
